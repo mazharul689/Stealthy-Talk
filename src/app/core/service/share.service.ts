8fe62e3bc6bd95b9a0dbb3c5b2aa6c145bcfe473
Md. Mazharul Islam <majharul689@gmail.com>
2024-09-10 18:42:45 +0600

    Images & logo updated

--- src/app/core/service/share.service.ts
+++ src/app/core/service/share.service.ts
@@ -6,9 +6,9 @@ import { BehaviorSubject } from 'rxjs'
 export class ShareService {
   cliLogo = 'assets/images/banner/DSS.png'
 
-  logoSrc = 'assets/images/banner/diit.png'
+  logoSrc = 'assets/images/banner/thunder-wolves.png'
   logoSrc1 = 'assets/images/banner/stgimg.png'
-  public messageSource = new BehaviorSubject<string>('Stego Image')
+  public messageSource = new BehaviorSubject<string>('Stealthy Talk')
   public clientLogo = new BehaviorSubject<any>(this.cliLogo)
   public logoSouce = new BehaviorSubject<any>(this.logoSrc)
   public logoSouce1 = new BehaviorSubject<any>(this.logoSrc1)
